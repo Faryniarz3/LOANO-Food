40e614112047886aa44036dc44b73f6c0fdf82ff
Oliver Gray <olliewgray@gmail.com>
2023-10-17 13:26:25 -0400

    ogray branch commit 2

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-const foodDescriptionList = document.getElementById('');
-const foodDescriptionListItems = foodDescriptionList.childNodes;
+//fetch('')
+
+// const foodDescriptionList = document.getElementById('');
+// const foodDescriptionListItems = foodDescriptionList.childNodes;
+const navImages = document.getElementsByClassName("nav-item");
+ navImages[0].addEventListener ('click',
+(event) =>
+console.log("CLICK")
+
+)
+navImages.forEach(element => {
+   element.addEventListener('click' , (event) => console.log(element)) 
+});
+ 
+
